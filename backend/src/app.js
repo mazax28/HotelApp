a2c7c67e3f6e21d9211038a8f676ee87d42402bd
mazax28 <marcoskrause2002@fpuna.edu.py>
2025-05-08 10:39:20 -0300

    feat: initialize Express application with CORS and route configurations

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import {clientRoutes} from './modules/clients/client.routes.js';
+import {roomRoutes} from './modules/rooms/room.routes.js';
+import {hotelRoutes} from './modules/hotels/hotel.routes.js';
+import {reservationRoutes} from './modules/reservations/reservation.routes.js';
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: 'http://localhost:5173', // o tu frontend real
+    credentials: true,
+
+}
+));
+
+app.use(express.json());
+
+app.use('/api/clients', clientRoutes);
+app.use('/api/rooms', roomRoutes);
+app.use('/api/hotels', hotelRoutes);
+app.use('/api/reservations', reservationRoutes);
+
+
+export default app;
